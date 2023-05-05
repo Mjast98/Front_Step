a4e8d17e3ba5ccd3bb08ca3a9d58283f3a199c4e
Mjast98 <mjastrzebskiwork@gmail.com>
2023-05-05 12:31:27 +0000

    Update

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@
 cd /home/mateusz/Documents/Git_FrontStep
 GIT_SSH_COMMAND="ssh -i /home/mateusz/.ssh/id_rsa" git add .
 GIT_SSH_COMMAND="ssh -i /home/mateusz/.ssh/id_rsa" git commit -m "Update"
-GIT_SSH_COMMAND="ssh -i /home/mateusz/.ssh/id_rsa" git push git@github.com:Mjast98/Library.git
+GIT_SSH_COMMAND="ssh -i /home/mateusz/.ssh/id_rsa" git push git@github.com:Mjast98/Front_Step.git
 
 
 
